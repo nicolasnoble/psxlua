d27cc4b64a279ed46ef9534fe4ba1a20a31a8ecb
Nicolas "Pixel" Noble <pixel@nobis-crew.org>
2026-09-20 20:45:59 -0700

    Modifying luac to build and run as a ps-exe
    Arguments as passed through pcsx-redux' debugging ability to hook into invalid memory reads, through the args.lua script, which needs to be loaded at the same time as running the luac.ps-exe binary.

---
+++ src/args.lua
@@ -0,0 +1,34 @@
+local ffi = require 'ffi'
+local uint8_t = ffi.typeof 'uint8_t[?]'
+local argsBuffer
+local processName = 'luac.ps-exe'
+
+function createArgsBuffer(...)
+    local args = { ... }
+    local spaceNeeded = #processName + 2
+    for i, v in ipairs(args) do
+        local a = tostring(v)
+        spaceNeeded = spaceNeeded + #a + 1
+    end
+    argsBuffer = ffi.new(uint8_t, spaceNeeded)
+    local ptr = ffi.cast('uint8_t *', argsBuffer)
+    ffi.copy(ptr, processName)
+    ptr = ptr + #processName + 1
+    for i, v in ipairs(args) do
+        local a = tostring(v)
+        ffi.copy(ptr, a)
+        ptr = ptr + #a + 1
+        spaceNeeded = spaceNeeded + #a + 1
+    end
+    ptr[0] = 0
+end
+
+function UnknownMemoryRead(address, size)
+    if size ~= 1 then return 0xffffffff end
+    local offset = address - 0x40000000
+    if offset < 0 then return 0xff end
+    if offset > ffi.sizeof(argsBuffer) then return 0xff end
+    return argsBuffer[offset]
+end
+
+createArgsBuffer()
--- src/luac.c
+++ src/luac.c
@@ -4,15 +4,20 @@
 ** See Copyright Notice in lua.h
 */
 
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #define luac_c
 #define LUA_CORE
+#define LUA_TARGET_PSX
+
+#include "common/hardware/pcsxhw.h"
+#include "common/kernel/pcdrv.h"
+#include "common/syscalls/syscalls.h"
+
+#define EXIT_FAILURE -1
+#define EXIT_SUCCESS 0
 
 #include "lauxlib.h"
+#include "lctype.h"
+#include "llibc.h"
 #include "lobject.h"
 #include "lstate.h"
 #include "lua.h"
@@ -32,21 +37,21 @@ static const char* output = Output;     /* actual output file name */
 static const char* progname = PROGNAME; /* actual program name */
 
 static void fatal(const char* message) {
-    fprintf(stderr, "%s: %s\n", progname, message);
-    exit(EXIT_FAILURE);
+    ramsyscall_printf("%s: %s\n", progname, message);
+    pcsx_exit(EXIT_FAILURE);
 }
 
 static void cannot(const char* what) {
-    fprintf(stderr, "%s: cannot %s %s: %s\n", progname, what, output, strerror(errno));
-    exit(EXIT_FAILURE);
+    ramsyscall_printf("%s: cannot %s %s\n", progname, what, output);
+    pcsx_exit(EXIT_FAILURE);
 }
 
 static void usage(const char* message) {
     if (*message == '-')
-        fprintf(stderr, "%s: unrecognized option " LUA_QS "\n", progname, message);
+        ramsyscall_printf("%s: unrecognized option " LUA_QS "\n", progname, message);
     else
-        fprintf(stderr, "%s: %s\n", progname, message);
-    fprintf(stderr,
+        ramsyscall_printf("%s: %s\n", progname, message);
+    ramsyscall_printf(
   "usage: %s [options] [filenames]\n"
   "Available options are:\n"
   "  -l       list (use -l -l for full listing)\n"
@@ -55,14 +60,13 @@ static void usage(const char* message) {
   "  -s       strip debug information\n"
   "  -v       show version information\n"
   "  --       stop handling options\n"
-  "  -        stop handling options and process stdin\n"
   ,progname,Output);
-    exit(EXIT_FAILURE);
+    pcsx_exit(EXIT_FAILURE);
 }
 
-#define IS(s) (strcmp(argv[i], s) == 0)
+#define IS(s) (luaA_strcmp(argv[i], s) == 0)
 
-static int doargs(int argc, char* argv[]) {
+static int doargs(int argc, const char* argv[]) {
     int i;
     int version = 0;
     if (argv[0] != NULL && *argv[0] != 0) progname = argv[0];
@@ -74,16 +78,13 @@ static int doargs(int argc, char* argv[]) {
             ++i;
             if (version) ++version;
             break;
-        } else if (IS("-")) /* end of options; use stdin */
-            break;
-        else if (IS("-l")) /* list */
+        } else if (IS("-l")) /* list */
             ++listing;
         else if (IS("-o")) /* output file */
         {
             output = argv[++i];
             if (output == NULL || *output == 0 || (*output == '-' && output[1] != 0))
                 usage(LUA_QL("-o") " needs argument");
-            if (IS("-")) output = NULL;
         } else if (IS("-p")) /* parse only */
             dumping = 0;
         else if (IS("-s")) /* strip debug information */
@@ -98,8 +99,8 @@ static int doargs(int argc, char* argv[]) {
         argv[--i] = Output;
     }
     if (version) {
-        printf("%s\n", LUA_COPYRIGHT);
-        if (version == argc - 1) exit(EXIT_SUCCESS);
+        ramsyscall_printf("%s\n", LUA_COPYRIGHT);
+        if (version == argc - 1) pcsx_exit(EXIT_SUCCESS);
     }
     return i;
 }
@@ -138,7 +139,8 @@ static const Proto* combine(lua_State* L, int n) {
 
 static int writer(lua_State* L, const void* p, size_t size, void* u) {
     UNUSED(L);
-    return (fwrite(p, size, 1, (FILE*)u) != 1) && (size != 0);
+    int r = PCwrite(*(int*)u, p, size);
+    return (r != size) && (size != 0);
 }
 
 LUALIB_API int(luaL_loadfilex)(lua_State* L, const char* filename, const char* mode);
@@ -147,7 +149,7 @@ LUALIB_API int(luaL_loadfilex)(lua_State* L, const char* filename, const char* m
 
 typedef struct LoadF {
     int n;                      /* number of pre-read characters */
-    FILE* f;                    /* file being read */
+    int f;                      /* file being read */
     char buff[LUAL_BUFFERSIZE]; /* area for reading file */
 } LoadF;
 
@@ -158,21 +160,26 @@ static const char* getF(lua_State* L, void* ud, size_t* size) {
         *size = lf->n; /* return them (chars already in buffer) */
         lf->n = 0;     /* no more pre-read characters */
     } else {           /* read a block from file */
-        /* 'fread' can return > 0 *and* set the EOF flag. If next call to
-           'getF' called 'fread', it might still wait for user input.
-           The next check avoids this problem. */
-        if (feof(lf->f)) return NULL;
-        *size = fread(lf->buff, 1, sizeof(lf->buff), lf->f); /* read block */
+        int read = PCread(lf->f, lf->buff, sizeof(lf->buff)); /* read block */
+        if (read <= 0) return NULL;
+        *size = read;
     }
     return lf->buff;
 }
 
 static int errfile(lua_State* L, const char* what, int fnameindex) {
-    const char* serr = strerror(errno);
     const char* filename = lua_tostring(L, fnameindex) + 1;
-    lua_pushfstring(L, "cannot %s %s: %s", what, filename, serr);
+    lua_pushfstring(L, "cannot %s %s", what, filename);
     lua_remove(L, fnameindex);
     return LUA_ERRFILE;
+}
+
+#define EOF -1
+
+static int getc(int f) {
+    int c = 0;
+    int r = PCread(f, &c, 1);
+    return r == 1 ? c : -1;
 }
 
 static int skipBOM(LoadF* lf) {
@@ -209,32 +216,21 @@ static int skipcomment(LoadF* lf, int* cp) {
 
 LUALIB_API int luaL_loadfilex(lua_State* L, const char* filename, const char* mode) {
     LoadF lf;
-    int status, readstatus;
+    int status;
     int c;
     int fnameindex = lua_gettop(L) + 1; /* index of filename on the stack */
     if (filename == NULL) {
-        lua_pushliteral(L, "=stdin");
-        lf.f = stdin;
+        return LUA_ERRFILE;
     } else {
-        lua_pushfstring(L, "@%s", filename);
-        lf.f = fopen(filename, "r");
-        if (lf.f == NULL) return errfile(L, "open", fnameindex);
+        const char * f = lua_pushfstring(L, "@%s", filename);
+        lf.f = PCopen(f + 1, 0, 0);
+        if (lf.f < 0) return errfile(L, "open", fnameindex);
     }
     if (skipcomment(&lf, &c))                 /* read initial portion */
         lf.buff[lf.n++] = '\n';               /* add line to correct line numbers */
-    if (c == LUA_SIGNATURE[0] && filename) {  /* binary file? */
-        lf.f = freopen(filename, "rb", lf.f); /* reopen in binary mode */
-        if (lf.f == NULL) return errfile(L, "reopen", fnameindex);
-        skipcomment(&lf, &c); /* re-read initial portion */
-    }
     if (c != EOF) lf.buff[lf.n++] = c; /* 'c' is the first character of the stream */
     status = lua_load(L, getF, &lf, lua_tostring(L, -1), mode);
-    readstatus = ferror(lf.f);
-    if (filename) fclose(lf.f); /* close file (even in case of errors) */
-    if (readstatus) {
-        lua_settop(L, fnameindex); /* ignore results from `lua_load' */
-        return errfile(L, "read", fnameindex);
-    }
+    PCclose(lf.f);
     lua_remove(L, fnameindex);
     return status;
 }
@@ -252,31 +248,40 @@ static int pmain(lua_State* L) {
     f = combine(L, argc);
     if (listing) luaU_print(f, listing > 1);
     if (dumping) {
-        FILE* D = (output == NULL) ? stdout : fopen(output, "wb");
-        if (D == NULL) cannot("open");
+        const char * fname = lua_pushfstring(L, "%s", output);
+        int D = PCcreat(fname, 0);
+        lua_pop(L, 1);
+        if (D < 0) cannot("open");
         lua_lock(L);
-        luaU_dump(L, f, writer, D, stripping);
+        luaU_dump(L, f, writer, &D, stripping);
         lua_unlock(L);
-        if (ferror(D)) cannot("write");
-        if (fclose(D)) cannot("close");
+        if (PCclose(D) != 0) cannot("close");
     }
     return 0;
 }
 
-int main(int argc, char* argv[]) {
+int main() {
+    const char * argv[64] = { 0 };
+    const char * argsPtr = (const char *) 0x40000000;
+    int argc = 0;
+    while (1) {
+        int len = luaA_strlen(argsPtr);
+        if (len == 0) break;
+        argv[argc++] = argsPtr;
+        argsPtr += len + 1;
+    }
     lua_State* L;
     int i = doargs(argc, argv);
     argc -= i;
-    argv += i;
     if (argc <= 0) usage("no input files given");
     L = luaL_newstate();
     if (L == NULL) fatal("cannot create state: not enough memory");
     lua_pushcfunction(L, &pmain);
     lua_pushinteger(L, argc);
-    lua_pushlightuserdata(L, argv);
+    lua_pushlightuserdata(L, argv + i);
     if (lua_pcall(L, 2, 0, 0) != LUA_OK) fatal(lua_tostring(L, -1));
     lua_close(L);
-    return EXIT_SUCCESS;
+    pcsx_exit(EXIT_SUCCESS);
 }
 
 /*
@@ -284,9 +289,6 @@ int main(int argc, char* argv[]) {
 ** print bytecodes
 ** See Copyright Notice in lua.h
 */
-
-#include <ctype.h>
-#include <stdio.h>
 
 #define luac_c
 #define LUA_CORE
@@ -300,64 +302,64 @@ int main(int argc, char* argv[]) {
 static void PrintString(const TString* ts) {
     const char* s = getstr(ts);
     size_t i, n = ts->tsv.len;
-    printf("%c", '"');
+    ramsyscall_printf("%c", '"');
     for (i = 0; i < n; i++) {
         int c = (int)(unsigned char)s[i];
         switch (c) {
             case '"':
-                printf("\\\"");
+                ramsyscall_printf("\\\"");
                 break;
             case '\\':
-                printf("\\\\");
+                ramsyscall_printf("\\\\");
                 break;
             case '\a':
-                printf("\\a");
+                ramsyscall_printf("\\a");
                 break;
             case '\b':
-                printf("\\b");
+                ramsyscall_printf("\\b");
                 break;
             case '\f':
-                printf("\\f");
+                ramsyscall_printf("\\f");
                 break;
             case '\n':
-                printf("\\n");
+                ramsyscall_printf("\\n");
                 break;
             case '\r':
-                printf("\\r");
+                ramsyscall_printf("\\r");
                 break;
             case '\t':
-                printf("\\t");
+                ramsyscall_printf("\\t");
                 break;
             case '\v':
-                printf("\\v");
+                ramsyscall_printf("\\v");
                 break;
             default:
-                if (isprint(c))
-                    printf("%c", c);
+                if (lisprint(c))
+                    ramsyscall_printf("%c", c);
                 else
-                    printf("\\%03d", c);
+                    ramsyscall_printf("\\%03d", c);
         }
     }
-    printf("%c", '"');
+    ramsyscall_printf("%c", '"');
 }
 
 static void PrintConstant(const Proto* f, int i) {
     const TValue* o = &f->k[i];
     switch (ttypenv(o)) {
         case LUA_TNIL:
-            printf("nil");
+            ramsyscall_printf("nil");
             break;
         case LUA_TBOOLEAN:
-            printf(bvalue(o) ? "true" : "false");
+            ramsyscall_printf(bvalue(o) ? "true" : "false");
             break;
         case LUA_TNUMBER:
-            printf(LUA_NUMBER_FMT, nvalue(o));
+            ramsyscall_printf(LUA_NUMBER_FMT, nvalue(o));
             break;
         case LUA_TSTRING:
             PrintString(rawtsvalue(o));
             break;
         default: /* cannot happen */
-            printf("? type=%d", ttype(o));
+            ramsyscall_printf("? type=%d", ttype(o));
             break;
     }
 }
@@ -378,61 +380,61 @@ static void PrintCode(const Proto* f) {
         int bx = GETARG_Bx(i);
         int sbx = GETARG_sBx(i);
         int line = getfuncline(f, pc);
-        printf("\t%d\t", pc + 1);
+        ramsyscall_printf("\t%d\t", pc + 1);
         if (line > 0)
-            printf("[%d]\t", line);
+            ramsyscall_printf("[%d]\t", line);
         else
-            printf("[-]\t");
-        printf("%-9s\t", luaP_opnames[o]);
+            ramsyscall_printf("[-]\t");
+        ramsyscall_printf("%-9s\t", luaP_opnames[o]);
         switch (getOpMode(o)) {
             case iABC:
-                printf("%d", a);
-                if (getBMode(o) != OpArgN) printf(" %d", ISK(b) ? (MYK(INDEXK(b))) : b);
-                if (getCMode(o) != OpArgN) printf(" %d", ISK(c) ? (MYK(INDEXK(c))) : c);
+                ramsyscall_printf("%d", a);
+                if (getBMode(o) != OpArgN) ramsyscall_printf(" %d", ISK(b) ? (MYK(INDEXK(b))) : b);
+                if (getCMode(o) != OpArgN) ramsyscall_printf(" %d", ISK(c) ? (MYK(INDEXK(c))) : c);
                 break;
             case iABx:
-                printf("%d", a);
-                if (getBMode(o) == OpArgK) printf(" %d", MYK(bx));
-                if (getBMode(o) == OpArgU) printf(" %d", bx);
+                ramsyscall_printf("%d", a);
+                if (getBMode(o) == OpArgK) ramsyscall_printf(" %d", MYK(bx));
+                if (getBMode(o) == OpArgU) ramsyscall_printf(" %d", bx);
                 break;
             case iAsBx:
-                printf("%d %d", a, sbx);
+                ramsyscall_printf("%d %d", a, sbx);
                 break;
             case iAx:
-                printf("%d", MYK(ax));
+                ramsyscall_printf("%d", MYK(ax));
                 break;
         }
         switch (o) {
             case OP_LOADK:
-                printf("\t; ");
+                ramsyscall_printf("\t; ");
                 PrintConstant(f, bx);
                 break;
             case OP_GETUPVAL:
             case OP_SETUPVAL:
-                printf("\t; %s", UPVALNAME(b));
+                ramsyscall_printf("\t; %s", UPVALNAME(b));
                 break;
             case OP_GETTABUP:
-                printf("\t; %s", UPVALNAME(b));
+                ramsyscall_printf("\t; %s", UPVALNAME(b));
                 if (ISK(c)) {
-                    printf(" ");
+                    ramsyscall_printf(" ");
                     PrintConstant(f, INDEXK(c));
                 }
                 break;
             case OP_SETTABUP:
-                printf("\t; %s", UPVALNAME(a));
+                ramsyscall_printf("\t; %s", UPVALNAME(a));
                 if (ISK(b)) {
-                    printf(" ");
+                    ramsyscall_printf(" ");
                     PrintConstant(f, INDEXK(b));
                 }
                 if (ISK(c)) {
-                    printf(" ");
+                    ramsyscall_printf(" ");
                     PrintConstant(f, INDEXK(c));
                 }
                 break;
             case OP_GETTABLE:
             case OP_SELF:
                 if (ISK(c)) {
-                    printf("\t; ");
+                    ramsyscall_printf("\t; ");
                     PrintConstant(f, INDEXK(c));
                 }
                 break;
@@ -446,41 +448,41 @@ static void PrintCode(const Proto* f) {
             case OP_LT:
             case OP_LE:
                 if (ISK(b) || ISK(c)) {
-                    printf("\t; ");
+                    ramsyscall_printf("\t; ");
                     if (ISK(b))
                         PrintConstant(f, INDEXK(b));
                     else
-                        printf("-");
-                    printf(" ");
+                        ramsyscall_printf("-");
+                    ramsyscall_printf(" ");
                     if (ISK(c))
                         PrintConstant(f, INDEXK(c));
                     else
-                        printf("-");
+                        ramsyscall_printf("-");
                 }
                 break;
             case OP_JMP:
             case OP_FORLOOP:
             case OP_FORPREP:
             case OP_TFORLOOP:
-                printf("\t; to %d", sbx + pc + 2);
+                ramsyscall_printf("\t; to %d", sbx + pc + 2);
                 break;
             case OP_CLOSURE:
-                printf("\t; %p", VOID(f->p[bx]));
+                ramsyscall_printf("\t; %p", VOID(f->p[bx]));
                 break;
             case OP_SETLIST:
                 if (c == 0)
-                    printf("\t; %d", (int)code[++pc]);
+                    ramsyscall_printf("\t; %d", (int)code[++pc]);
                 else
-                    printf("\t; %d", c);
+                    ramsyscall_printf("\t; %d", c);
                 break;
             case OP_EXTRAARG:
-                printf("\t; ");
+                ramsyscall_printf("\t; ");
                 PrintConstant(f, ax);
                 break;
             default:
                 break;
         }
-        printf("\n");
+        ramsyscall_printf("\n");
     }
 }
 
@@ -495,32 +497,32 @@ static void PrintHeader(const Proto* f) {
         s = "(bstring)";
     else
         s = "(string)";
-    printf("\n%s <%s:%d,%d> (%d instruction%s at %p)\n", (f->linedefined == 0) ? "main" : "function", s, f->linedefined,
+    ramsyscall_printf("\n%s <%s:%d,%d> (%d instruction%s at %p)\n", (f->linedefined == 0) ? "main" : "function", s, f->linedefined,
            f->lastlinedefined, S(f->sizecode), VOID(f));
-    printf("%d%s param%s, %d slot%s, %d upvalue%s, ", (int)(f->numparams), f->is_vararg ? "+" : "", SS(f->numparams),
+    ramsyscall_printf("%d%s param%s, %d slot%s, %d upvalue%s, ", (int)(f->numparams), f->is_vararg ? "+" : "", SS(f->numparams),
            S(f->maxstacksize), S(f->sizeupvalues));
-    printf("%d local%s, %d constant%s, %d function%s\n", S(f->sizelocvars), S(f->sizek), S(f->sizep));
+    ramsyscall_printf("%d local%s, %d constant%s, %d function%s\n", S(f->sizelocvars), S(f->sizek), S(f->sizep));
 }
 
 static void PrintDebug(const Proto* f) {
     int i, n;
     n = f->sizek;
-    printf("constants (%d) for %p:\n", n, VOID(f));
+    ramsyscall_printf("constants (%d) for %p:\n", n, VOID(f));
     for (i = 0; i < n; i++) {
-        printf("\t%d\t", i + 1);
+        ramsyscall_printf("\t%d\t", i + 1);
         PrintConstant(f, i);
-        printf("\n");
+        ramsyscall_printf("\n");
     }
     n = f->sizelocvars;
-    printf("locals (%d) for %p:\n", n, VOID(f));
+    ramsyscall_printf("locals (%d) for %p:\n", n, VOID(f));
     for (i = 0; i < n; i++) {
-        printf("\t%d\t%s\t%d\t%d\n", i, getstr(f->locvars[i].varname), f->locvars[i].startpc + 1,
+        ramsyscall_printf("\t%d\t%s\t%d\t%d\n", i, getstr(f->locvars[i].varname), f->locvars[i].startpc + 1,
                f->locvars[i].endpc + 1);
     }
     n = f->sizeupvalues;
-    printf("upvalues (%d) for %p:\n", n, VOID(f));
+    ramsyscall_printf("upvalues (%d) for %p:\n", n, VOID(f));
     for (i = 0; i < n; i++) {
-        printf("\t%d\t%s\t%d\t%d\n", i, UPVALNAME(i), f->upvalues[i].instack, f->upvalues[i].idx);
+        ramsyscall_printf("\t%d\t%s\t%d\t%d\n", i, UPVALNAME(i), f->upvalues[i].instack, f->upvalues[i].idx);
     }
 }
 
@@ -530,4 +532,9 @@ static void PrintFunction(const Proto* f, int full) {
     PrintCode(f);
     if (full) PrintDebug(f);
     for (i = 0; i < n; i++) PrintFunction(f->p[i], full);
+}
+
+int luaI_sprintf(char *str, const char *format, ...) {
+    *str = 0;
+    return 0;
 }
